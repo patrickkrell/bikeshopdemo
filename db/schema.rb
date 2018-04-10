d1781482cf8339f377c181576710ce2f2eaa28bb
Patrick Krell <patrick.krell@googlemail.com>
2018-04-10 09:40:05 +0200

    4.2 Bugfixes updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409080522) do
+ActiveRecord::Schema.define(version: 20180410072753) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180409080522) do
     t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "colour"
+    t.decimal "price"
   end
 
 end
