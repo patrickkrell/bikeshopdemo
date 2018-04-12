6411f0f97ebf3441ef0c8b7d0966856508a5e4ad
Patrick Krell <patrick.krell@googlemail.com>
2018-04-12 09:05:05 +0200

    4.3 Navigation Products added, added picture landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products
   resources	:orders, only: [:index, :show, :create, :destroy] 
+  get 'simple_page/products' 
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
