fe2f1a17267e1db8f4780298bc7d0eed83e3b108
Patrick Krell <patrick.krell@googlemail.com>
2018-04-12 14:56:19 +0200

    4.6 Orders, index and show for orders, styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
   resources	:orders, only: [:index, :show, :create, :destroy] 
   get 'simple_page/products' 
-
+  get 'simple_page/orders'
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
