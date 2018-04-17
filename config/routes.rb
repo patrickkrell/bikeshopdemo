a0cf60a096f6def576bec201d4ee50caffe93b25
Patrick Krell <patrick.krell@googlemail.com>
2018-04-17 10:38:05 +0200

    5.1 Reroute root page, redirect users, added dynamic links, nav update

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :products
   resources	:orders, only: [:index, :show, :create, :destroy] 
+
   get 'simple_page/products' 
   get 'simple_page/orders'
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
 
-  get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
+  # root 'simple_pages#landing_page'
+  get 'simple_pages/landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
