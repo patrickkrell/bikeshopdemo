7a6c1764a8572c7bdb6f7b061c0d85e0587d6157
Patrick Krell <patrick.krell@googlemail.com>
2018-04-18 10:48:56 +0200

    5.2 search function, css styling for search function, added buttons for forms

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  root 'simple_pages#index'
-  # root 'simple_pages#landing_page'
-  get 'simple_pages/landing_page'
+  get 'simple_pages/index'
+  
+  root 'simple_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
