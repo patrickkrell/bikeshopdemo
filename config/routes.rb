a179b2b51aa5c3aa42bc181c0510be1cc715287d
Patrick Krell <patrick.krell@googlemail.com>
2018-04-24 11:02:04 +0200

    5.6 Branch Comment created

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,12 @@ Rails.application.routes.draw do
   resources :products
   resources	:orders, only: [:index, :show, :create, :destroy] 
 
-  get 'simple_page/products' 
-  get 'simple_page/orders'
+  get 'simple_pages/index'
   get 'simple_pages/about'
-
   get 'simple_pages/contact'
 
-  get 'simple_pages/index'
+  get 'simple_page/products' 
+  get 'simple_page/orders'
   
   root 'simple_pages#landing_page'
 
