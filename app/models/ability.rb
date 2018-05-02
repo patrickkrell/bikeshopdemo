8f3b97d6a137d00b4dcb540c1c53c4e075b642f5
Patrick Krell <patrick.krell@googlemail.com>
2018-05-02 10:31:31 +0200

    5.10 changes ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,9 @@ class Ability
     can :read, :all .
     if user.admin?  
       can :manage, :all
-    else
-      can :manage, User, id: user.id  
+      if
+        can :manage, User, id: user.id  
+      end
     end
-  end
+  end  
 end
