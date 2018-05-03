4e94476c2d00ed770ca9d3a7f0dfce18e7b767fd
Patrick Krell <patrick.krell@googlemail.com>
2018-05-03 09:11:40 +0200

    5.11 sign-up email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
       to: 'mail@patrickkrell.de',
       subject: "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  @appname = "Bike Shop Demo"
+  mail(to: user.email,
+       subject: "Welcome to #{@appname}!")
+  end
+
 end
