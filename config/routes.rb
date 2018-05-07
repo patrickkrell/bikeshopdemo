e4161cd2d3717311bebb846f939545953c50d287
Patrick Krell <patrick.krell@googlemail.com>
2018-05-07 18:01:34 +0200

    payments controller routes adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,13 @@ Rails.application.routes.draw do
   get 'simple_page/products' 
   get 'simple_page/orders'
 
-  root 'simple_pages#landing_page'
 
   post 'simple_pages/thank_you'
+
+  post 'payments/create'
+  get 'payments/create'
+
+  root 'simple_pages#landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
