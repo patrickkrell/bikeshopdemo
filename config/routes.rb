c0bfe69b0e14e2e392fbdde84ee108cb804f1920
Patrick Krell <patrick.krell@googlemail.com>
2018-05-11 16:41:30 +0200

    6.13 ActionCable added for new comments

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,7 @@ Rails.application.routes.draw do
 
   root 'simple_pages#landing_page'
 
+  mount ActionCable.server => '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
